e20751ccccb181a3d7c9623eda112680478b927a
Shahab Jalalvand <shahabjld2@gmail.com>
2016-04-07 11:29:58 +0200

    Added files via upload

---
+++ fast_run_MLR.sh
@@ -0,0 +1,29 @@
+#/bin/bash run the QE process on multiple microphones
+
+
+export BINDIR=`pwd`
+export BASEDIR=`pwd`
+export RANKLIBDIR=/home/jalalvand/Desktop/toolkits/RankLib-v2.1/bin # in order to use MLR models you need to have RankLib library 
+
+if [ ! -e /home/jalalvand/Desktop/toolkits/RankLib-v2.1/bin/RankLib.jar ];then
+  printf "ERROR!!! RankLib library not found in \"$RANKLIBDIR\"\n"
+  return
+fi
+
+export CHANNELS=5  # Since there are 5 transcription channels in the data files
+
+
+mkdir -p $BASEDIR/temp
+mkdir -p $BASEDIR/temp/MLR
+
+
+export MLR_Tune=No  # Set it to "Yes" if you want to optimize the parameters on Training set
+
+# ./MLR_train.sh train_data  k-fold  models
+. $BINDIR/MLR-QE/MLR_train.sh  $BASEDIR/data/MLR_train_LEX_LM_POS.data 5 MLR_models
+
+# ./MLR_test.sh  test_data  models  output
+. $BINDIR/MLR-QE/MLR_test.sh   $BASEDIR/data/MLR_test_LEX_LM_POS.data MLR_models $BASEDIR/data/MLR_output.prank
+
+
+
