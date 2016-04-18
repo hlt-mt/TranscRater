547c19aaebac9007331f3ba9618751cfd8a2d7be
Shahab Jalalvand <shahabjld2@gmail.com>
2016-04-18 15:14:30 +0200

    Added files via upload

--- bin/run_config.sh
+++ bin/run_config.sh
@@ -22,8 +22,82 @@ done
 export Feat=${ffeat/_/}
 
 if [[ $Feat = "" ]]; then
-  echo "Error!!! No Feature is defined"
+  printf "Error!!! No Feature is defined...\n"
   return
 fi
 
-#rm ${inputfile}.exp
+rm ${inputfile}.exp
+
+
+# User's configuration problem
+
+# Transciprion files
+for transc in `echo ${train_transcChannels[*]}`; do
+  if [ ! -e $transc ]; then
+    printf "ERROR!!! Transcription file $transc does not exist...\n"
+    return
+  fi
+done
+for transc in `echo ${test_transcChannels[*]}`; do
+  if [ ! -e $transc ]; then
+    printf "ERROR!!! Transcription file $transc does not exist...\n"
+    return
+  fi
+done
+
+
+# Feature Selection
+
+# Lexicon Features
+if [ $F_LEX = 1 ]; then
+  if [ ! -e $LEXFEAT ]; then
+    printf "ERROR!!! You asked for Lexicon based features but $LEXFEAT does not exist...\n"
+    return
+  fi
+fi
+
+# Language model features
+if [ $F_LM = 1 ]; then
+  if [ ! -e $RNNLMDIR/rnnlm ]; then
+    printf "ERROR!!! You asked for LM features but $RNNLMDIR/rnnlm does not exist...\n"
+    return
+  fi
+  if [ ! -e $SRILMDIR/ngram ]; then
+    printf "ERROR!!! You asked for LM features but $SRILMDIR does not exist...\n"
+    return
+  fi
+fi
+
+# Part-of-speech features
+if [ $F_POS = 1 ]; then
+  if [ ! -e $TREETAGDIR/tree-tagger ]; then
+    printf "ERROR!!! You asked for POS based features but $TREETAGDIR/tree-tagger does not exist...\n"
+    return
+  fi
+fi
+
+# Signal based features
+if [ $F_SIG = 1 ]; then
+  if [ ! -e $OPENSMILEDIR/SMILExtract ]; then
+    printf "Error!!! You asked for Signal features but OpenSmile tool is not ready in $OPENSMILEDIR/SMILExtract...\n"
+    return
+  fi
+  for sig_list in `echo ${train_wavChannels[*]}`; do
+    if [ ! -e $sig_list ]; then
+      printf "Error!!! You asked for Signal features but $sig_list does not exist...\n"
+      return;
+    fi
+  done
+  for sig_list in `echo ${test_wavChannels[*]}`; do
+    if [ ! -e $sig_list ]; then
+      printf "Error!!! You asked for Signal features but $sig_list does not exist...\n"
+      return;
+    fi
+  done
+fi
+
+
+# Machine Learninig
+
+
+      
