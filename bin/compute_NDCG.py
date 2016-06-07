567704d8df6f99355a178d1780a62b8558ac16b6
Shahab Jalalvand <shahabjld2@gmail.com>
2016-06-07 15:59:04 +0200

    Add files via upload

--- bin/compute_NDCG.py
+++ bin/compute_NDCG.py
@@ -8,49 +8,38 @@ import argparse
 import math
 import numpy as np
 
-if sys.argv[1]:
-  true_rank = sys.argv[1]
-else:
-  print "Error!!! Not defined inputs for ./bin/compute_NDCG.py ..."
   
-if sys.argv[2]:
-  pred_rank = sys.argv[2]
-else:
-  print "Error!!! Not defined inputs for ./bin/compute_NDCG.py ..."
+def main(true_rank_mat, pred_rank_mat):
 
+  if ( true_rank_mat.shape != pred_rank_mat.shape ):
+    print "Error!!! Mismatch between True and Predicted ranking files shape"
+    exit()
 
-true_rank_mat = np.nan_to_num(np.genfromtxt(true_rank, delimiter=' '))
-pred_rank_mat = np.nan_to_num(np.genfromtxt(pred_rank, delimiter=' '))
+  ndcg=[]
 
-if ( true_rank_mat.shape != pred_rank_mat.shape ):
-  print "Error!!! Mismatch between True and Predicted ranking files shape"
-  exit()
+  for k in range(true_rank_mat.shape[0]):
+    trtmp = true_rank_mat.shape[1] - true_rank_mat[k][true_rank_mat[k].ravel().argsort()] + 1
+    prtmp = true_rank_mat.shape[1] - pred_rank_mat[k][true_rank_mat[k].ravel().argsort()] + 1
 
-
-
-ndcg=[]
-
-for k in range(true_rank_mat.shape[0]):
-  trtmp = true_rank_mat.shape[1] - true_rank_mat[k][true_rank_mat[k].ravel().argsort()] + 1
-  prtmp = true_rank_mat.shape[1] - pred_rank_mat[k][true_rank_mat[k].ravel().argsort()] + 1
-
-  idcg = 0
-  i=1
-  for elem in trtmp:
-    idcg += (math.pow(2,elem) - 1 ) / math.log(i+1,2)
-    i+=1
+    idcg = 0
+    i=1
+    for elem in trtmp:
+      idcg += (math.pow(2,elem) - 1 ) / math.log(i+1,2)
+      i+=1
+      
+    dcg = 0
+    i=1
+    for elem in prtmp:
+      dcg += (math.pow(2,elem) - 1 ) / math.log(i+1,2)
+      i+=1
     
-  dcg = 0
-  i=1
-  for elem in prtmp:
-    dcg += (math.pow(2,elem) - 1 ) / math.log(i+1,2)
-    i+=1
-  
-#  print  (dcg / idcg)
-  ndcg.append( dcg / idcg )
-  
-print "NDCG: %.3f" % np.mean(ndcg*100)
+  #  print  (dcg / idcg)
+    ndcg.append( dcg / idcg )
+    
+  print "NDCG : %.3f" % np.mean(ndcg*100)
 
 
+if __name__=='__main__':
+  sys.exit(main(sys.argv[1], sys.argv[2]))
 
 
