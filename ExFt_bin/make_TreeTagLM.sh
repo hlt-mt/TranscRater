25124cbcd60303cfe742c73e55d4833e8a88257e
Shahab Jalalvand <shahabjld2@gmail.com>
2016-06-09 17:19:43 +0200

    Delete make_TreeTagLM.sh

--- ExFt_bin/make_TreeTagLM.sh
+++
@@ -1,8 +0,0 @@
-
-bbdir=/hltsrv2/jalalvand/Word-based_Feature_Extraction_Package
-ppp="$bbdir/TreeTagger"
-input=$1
-cat $input | while read l; 
-do 
-  echo $l | tr ' ' '\n' | $ppp/bin/tree-tagger $ppp/lib/english.par -cap-heuristics | tr '\n' ' '; echo ""
-done > ${input}.pos
