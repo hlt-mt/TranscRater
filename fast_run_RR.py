fe143e1dbea3dd6135308a12219a505457ae959f
Shahab Jalalvand <shahabjld2@gmail.com>
2016-06-10 17:23:41 +0200

    Add files via upload

--- fast_run_RR.py
+++ fast_run_RR.py
@@ -13,21 +13,22 @@ sys.path.insert(0, BINDIR+"/REG-QE")
 with open("config.json", 'r') as f:
    config = json.load(f)
 
-train_file  = config['BASEDIR']+"/data/MLR_train_"+config['FEAT']+".data"
-test_file   = config['BASEDIR']+"/data/MLR_test_"+config['FEAT']+".data"
-models      = config['BASEDIR']+"/MLR_models"
-output_file = config['BASEDIR']+"/resutls/test.pred"
+traindir  = config['BASEDIR']+"/data/features"
+testdir   = config['BASEDIR']+"/data/features"
+modelsdir = config['BASEDIR']+"/MLR_models"
+out_file  = config['BASEDIR']+"/results/test.pwer"
 
 
 if not os.path.exists( config['BASEDIR']+"/temp" ):
   os.makedirs( config['BASEDIR']+"/temp" )
-
+if not os.path.exists( config['BASEDIR']+"/results" ):
+  os.makedirs( config['BASEDIR']+"/results" )
 
 
 import RR_train
-RR_train.main( config['BASEDIR']+"/data", config['BASEDIR']+"/RR_models" )
+RR_train.main( traindir, modelsdir )
 
 import RR_test
-RR_test.main( config['BASEDIR']+"/data", config['BASEDIR']+"/RR_models" )
+RR_test.main( testdir, modelsdir, out_file )
 
 
