46fa54758615d0efdee8b63bae644cc09f47253f
Shahab Jalalvand <shahabjld2@gmail.com>
2016-06-29 16:19:51 +0200

    Add files via upload

--- MLR-QE/MLR_train.py
+++ MLR-QE/MLR_train.py
@@ -13,30 +13,28 @@ import errno
 
 def tune_MLR(RANKLIBDIR,trainfile,folds):
     print 'MLR parameter tuning ...'
-    bg = [10 ,50 ,100]
-    tr = [5, 10, 20]
-    lf = [5, 10, 20]
-    f = open(config['BASEDIR']+"/temp/MLR/train.log",'w+')
-
-    for bgi in range(len(bg) ):
-        for tri in range(len(tr)):
-            for lfi in range(len(lf)):
-                line= '-ranker '+str(type)+' -bag '+str(bg[bgi])+' -tree '+str(tr[tri])+' -leaf '+str(lf[lfi])+' '
-                f.write(line+os.linesep)
-                args = [ RANKLIBDIR+"/RankLib-2.6.jar", "-train", trainfile, "-ranker", type, 
-                         "-bag", bg, "-tree", tr, "-leaf", lf, "-srate", "0.1", "-frate", "0.5", 
-                         "-shrinkage", "0.5", "-tc", "1", "-mls", "10","-kcv", folds, "-norm", 
-                         "zscore",  "-metric2t", "NDCG@${CHANNELS} -metric2T NDCG@${CHANNELS}" ]
-                subprocess.Popen('java -jar'+(''.join(list(str(" "+e) for e in args))), shell=True, stdout=subprocess.PIPE).stdout.read()
-                f.write(line+os.linesep)
-                
-                
-    f.close()
     
-    best_parameters='cat $BASEDIR/temp/MLR/train.log | sort -n -k 9 -r | head -n 1 | cut -d\' \' -f-8'  
+    log = []
+    
+    for bg in [10 ,50 ,100]:
+        for tr in [5, 10, 20]:
+            for lf in [5, 10, 20]:             
+                parameters= '-ranker 8 -bag '+str(bg)+' -tree '+str(tr)+' -leaf '+str(lf)+' -rtype 6'
+                print parameters,
+                args=[config['RANKLIBDIR']+"/RankLib-2.6.jar", "-train", trainfile,
+                        parameters,
+                        "-srate", "0.1", "-frate", "0.5", "-shrinkage", "0.5" ,"-tc" ,"1" , "-mls", "10",
+                        "-kcv", "5", "-norm zscore -metric2t NDCG@10 -metric2T NDCG@10 | grep Total | awk '{print $NF}' " ]
+                proc = subprocess.Popen('java -jar'+(''.join(list(str(" "+e) for e in args))), shell=True, stdout=subprocess.PIPE)
+                partial_ndcg = proc.stdout.read()
+                log.append((parameters, partial_ndcg))
+                print partial_ndcg
                     
-        
-        
+    best_parameters = sorted(log, key=lambda log: log[1],reverse=True)[0][0]
+    print "best found parameters: %s" % best_parameters    
+    return  best_parameters
+
+
 def main(trainfile, modelsdir):
 
    if not os.path.exists( config['BASEDIR']+"/temp" ):
@@ -48,14 +46,14 @@ def main(trainfile, modelsdir):
 
    # ----------------- Train/valid RR models
 
-   if config['MLR_Tune'] == "Yes":
-       tune_MLR(config['RANKLIBDIR'],trainfile,folds)
+   if int(config['MLR_Tune']) :
+       best_parameters= tune_MLR(config['RANKLIBDIR'], trainfile, config['folds'])
     
    print "\nTrain MLR models on \""+trainfile+"\" with best parameters of \""+best_parameters+"\" ..."
 
    trainData=trainfile 
    
-   args=[config['RANKLIBDIR']+"/RankLib-2.6.jar","-train",trainData,best_parameters,
+   args=[config['RANKLIBDIR']+"/RankLib-2.6.jar", "-train", trainData, best_parameters,
                 "-srate", "0.1", "-frate", "0.5", "-shrinkage", "0.5" ,"-tc" ,"1" ,
                 "-mls" ,"10","-norm zscore" ,"-metric2t", "NDCG@"+config['CHANNELS'] ,
                 "-save",modelsdir+"/MLR.model 2>&1 | grep WriteNothing"]
